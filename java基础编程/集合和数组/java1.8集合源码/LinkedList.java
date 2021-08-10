6880085668586bd1117aa57dc584a28fb068a3c5
hecaigui <982791027@qq.com>
2021-08-10 20:40:21 +0800

    feat：java集合初研究

---
+++ java基础编程/集合和数组/java1.8集合源码/LinkedList.java
@@ -0,0 +1,36 @@
+    /**
+     * Pointer to last node.
+     * Invariant: (first == null && last == null) ||
+     *            (last.next == null && last.item != null)
+     */
+    transient Node<E> last;
+    private static class Node<E> {
+        E item;
+        Node<E> next;
+        Node<E> prev;
+
+        Node(Node<E> prev, E element, Node<E> next) {
+            this.item = element;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+新增：
+    public boolean add(E e) {
+        linkLast(e);
+        return true;
+    }
+	/**
+     * Links e as last element.
+     */
+    void linkLast(E e) {
+        final Node<E> l = last;
+        final Node<E> newNode = new Node<>(l, e, null);
+        last = newNode;
+        if (l == null)
+            first = newNode;
+        else
+            l.next = newNode;
+        size++;
+        modCount++;
+    }
